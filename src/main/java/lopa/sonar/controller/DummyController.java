3dc28ce40dd541dac6938a0e53c4a42e868eeea7
Dvir, Yonit <yonit.dvir@emc.com>
2019-06-02 15:20:04 +0300

    add dummy project for sonar integration demonstration

---
+++ src/main/java/lopa/sonar/controller/DummyController.java
@@ -0,0 +1,23 @@
+package lopa.sonar.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController {
+
+    @Value("${service.version}")
+    private String version;
+
+    @GetMapping()
+    private Map<String, String> getVersion() {
+        Map<String, String> result = new HashMap<>();
+        result.put("version", version);
+        return result;
+    }
+}
